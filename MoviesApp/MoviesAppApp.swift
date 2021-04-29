8f22ffb0f93557c40a4431ae165a12328f2e508b
TrentonLazorchak <trentonlaz@gmail.com>
2021-04-29 13:58:49 -0400

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Trenton Lazorchak on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
